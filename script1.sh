115761ca4fc850e10e0dcf72f83554f39af4ca37
scseekers <72568403+scseekers@users.noreply.github.com>
2021-04-07 00:02:42 +0530

    Update script1.sh

--- script1.sh
+++ script1.sh
@@ -1,13 +1,26 @@
+############### To be Changed if required ######################
+
+echo "export http_proxy="10.10.78.61:3128"
+export https_proxy="10.10.78.61:3128"" >> ~/.bashrc
+################################################################
+
+
+####### Don't touch the code below until you are smart enough, boy!! ############################
+
+
+########few more aliases #########
 echo "# some more ls aliases
 alias ll='ls -alF'
 alias la='ls -A'
 alias l='ls -CF'" >> ~/.bashrc
 
 echo -e "\n" >> ~/.bashrc
+###################################
 
-echo "export http_proxy="10.10.78.61:3128"
-export https_proxy="10.10.78.61:3128"" >> ~/.bashrc
 
+
+
+######### Installing and activating miniconda ############################################
 source ~/.bashrc
 wget https://repo.anaconda.com/miniconda/Miniconda3-py39_4.9.2-Linux-x86_64.sh -O miniconda.sh \
 && chmod +x miniconda.sh && bash miniconda.sh -b -p miniconda
@@ -18,3 +31,5 @@ export PATH=$base_dir/miniconda/bin:$PATH
 source ~/.bashrc
 echo -e "$base_dir/miniconda/etc/profile.d/conda.sh" >> ~/.profile
 conda init bash
+
+#########################################################################################
