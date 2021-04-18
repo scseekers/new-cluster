6c9c648dfe3edd145acf31bb9919aaf9c4f10b7a
scseekers <72568403+scseekers@users.noreply.github.com>
2021-04-18 12:56:33 +0530

    Update pkg.R

--- pkg.R
+++ pkg.R
@@ -6,19 +6,19 @@ BiocManager::install(c('qvalue','plot3D','ggplot2','pheatmap','cowplot',
                       'ggbio', 'Gviz', 'DESeq2', 'RUVSeq',
                       'gProfileR', 'ggfortify', 'corrplot',
                       'gage', 'EDASeq', 'citr', 'formatR',
-                      'svglite', 'Rqc', 'ShortRead', 'QuasR',
-                      'methylKit','FactoMineR', 'iClusterPlus',
+                      'svglite', 'Rqc', 'ShortRead',
+                      'methylKit','FactoMineR',
                       'enrichR','caret','xgboost','glmnet',
                       'DALEX','kernlab','pROC','nnet','RANN',
                       'ranger','GenomeInfoDb', 'GenomicRanges',
                       'GenomicAlignments', 'ComplexHeatmap', 'circlize', 
                       'rtracklayer', 'BSgenome.Hsapiens.UCSC.hg38',
-                      'BSgenome.Hsapiens.UCSC.hg19','tidyr',
+                      'BSgenome.Hsapiens.UCSC.hg19',"BSgenome.Hsapiens.UCSC.hg38",'tidyr',
                       'AnnotationHub', 'GenomicFeatures', 'normr',
                       'MotifDb', 'TFBSTools', 'rGADEM', 'JASPAR2018','annotatr','biomaRt',"hugene20sttranscriptcluster.db",
                       'org.Hs.eg.db','AnnotationDbi','TxDb.Hsapiens.UCSC.hg19.knownGene','Homo.sapiens',"genomation",
-                      'GenomeInfoDbData','gkmSVM','DEXSeq',"rnaseqDTU","ASpli"
+                      'GenomeInfoDbData','gkmSVM','DEXSeq',"rnaseqDTU","ASpli",'ngsReports',"SummarizedExperiment"
 					  
                      ))
 
-install.packages(c("shiny", "shinythemes"))
+#install.packages(c("shiny", "shinythemes"))
